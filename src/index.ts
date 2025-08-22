0fdfb890f1bc6b43733449b61c4725b44631404a
Felix Schabe <felix.schabe@edeka.de>
2025-08-22 11:56:47 +0200

    basicAuth

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-import { R2Explorer } from "r2-explorer";
+import {R2Explorer} from "r2-explorer";
 
 export default R2Explorer({
-  // Set to false to allow users to upload files
-  readonly: true,
-
-  // Learn more how to secure your R2 Explorer instance:
-  // https://r2explorer.com/getting-started/security/
-  // cfAccessTeamName: "my-team-name",
+    readonly: true,
+    basicAuth: [{
+        username: 'felix',
+        password: 'ZtTozIHDlr'
+    }]
 });
